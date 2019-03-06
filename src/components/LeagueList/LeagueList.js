e12807d84a564d591a7485091863accddd0819c8
Andrew Henderson <andrew.henderson@coxautoinc.com>
2019-03-06 17:03:14 -0500

    continued work on league feed

--- src/components/LeagueList/LeagueList.js
+++ src/components/LeagueList/LeagueList.js
@@ -1,23 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import useFetch from "../utils/useFetch";
 // import _ from "lodash"
 
 const LeagueList = () => {
   const query = `http://api.pathofexile.com/leagues?type=main`;
   const result = useFetch(query, []);
-
-  useEffect(() => {
-    console.log("result", result);
-  });
-
-  // const leagueItems = result.map((league, index) => (
-  //   <li key="index">{league}</li>
-  // ));
+  // const leagues = result.map(obj => { return obj })
+  const leagues = result.map((league) => 
+    <React.Fragment>
+      <li>{league.id}</li>
+      <ul>
+        {league.rules.map((rules) => 
+          <li>{rules.description}</li>
+        )}
+      </ul>
+    </React.Fragment>
+  );
 
   return (
     <div>
-      <span>{JSON.stringify(result, "id", 2)}</span>
-      {/* <ul>{leagueItems}</ul> */}
+      {console.log(JSON.stringify(result, "id", 2))}
+      <ul>{leagues}</ul>
     </div>
   );
 };
